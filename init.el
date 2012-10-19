c8e92502e47007b28f7b4f50d32ca1738ef07a29
Rock Lee <xinli@redhat.com>
2012-10-19 09:36:43 +0800

    update emacs configuration

--- init.el
+++ init.el
@@ -63,29 +63,40 @@
 
 
 ;;; Fullscreen and maximized
+
 (defun my-fullscreen ()
   (interactive)
-  (x-send-client-message
-   nil 0 nil "_NET_WM_STATE" 32
-   '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
+  (if (not (string= system-type "darwin"))
+      (x-send-client-message
+       nil 0 nil "_NET_WM_STATE" 32
+       '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
+  )
+
 (defun my-maximized ()
   (interactive)
-  (x-send-client-message
-   nil 0 nil "_NET_WM_STATE" 32
-   '(1 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
-  (interactive)
-  (x-send-client-message
-   nil 0 nil "_NET_WM_STATE" 32
-   '(1 "_NET_WM_STATE_MAXIMIZED_VERT" 0)))
+  (if (not (string= system-type "darwin"))
+    (x-send-client-message
+     nil 0 nil "_NET_WM_STATE" 32
+     '(1 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+    (interactive)
+    (x-send-client-message
+     nil 0 nil "_NET_WM_STATE" 32
+     '(1 "_NET_WM_STATE_MAXIMIZED_VERT" 0)))
+  )
+
 (my-maximized)
 (my-fullscreen)
+
+;;;Window Numbering, Lets M 1 switch window
+(require 'window-numbering)
+(window-numbering-mode 1)
 
 ;;;Key Bindings
 (global-set-key (kbd "<f2>") 'rename-buffer)
 (global-set-key (kbd "C-c z") 'shell)
 (global-set-key (kbd "<f10>") 'my-fullscreen)
 
-;;;Google Maps
-(add-to-list 'load-path "~/.emacs.d/google-maps")
-(require 'google-maps)
+(require 'rock-erc)
 
+;;;LaTex
+(require 'rock-latex)
